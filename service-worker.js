abeab09c9125151b0e1c9656d16c9c6e109d7453
a355226 <a355226@hotmail.com>
2025-06-13 20:15:25 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,26 @@
+const CACHE_NAME = 'longterm-calc-cache-v1';
+const urlsToCache = [
+  './',
+  './index.html',
+  './icon.png',
+  './manifest.json'
+];
+
+// 安裝時快取資源
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+// 拦截 fetch 請求
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      // 優先使用快取，否則嘗試網路請求
+      return response || fetch(event.request);
+    })
+  );
+});
